07af13f53bb93494ac9e79d5a5bb0d772a4d7da4
panhaoyu <panhaoyu.china@outlook.com>
2024-10-04 13:37:48 +0800

    Fix typehint

--- src/addon.ts
+++ src/addon.ts
@@ -3,35 +3,43 @@ import { DialogHelper } from "zotero-plugin-toolkit/dist/helpers/dialog";
 import hooks from "./hooks";
 import { createZToolkit } from "./utils/ztoolkit";
 
+type Environment = "development" | "production";
+
+interface Locale {
+  current: any;
+}
+
+interface Prefs {
+  window: Window;
+  columns: Array<ColumnOptions>;
+  rows: Array<Record<string, string>>;
+}
+
+interface Data {
+  alive: boolean;
+  env: Environment;
+  ztoolkit: CustomZoteroToolkit;
+  locale?: Locale;
+  prefs?: Prefs;
+  dialog?: DialogHelper;
+}
+
 export default class Addon {
-  public data: {
-    alive: boolean;
-    // Env type, see build.js
-    env: "development" | "production";
-    ztoolkit: CustomZoteroToolkit;
-    locale?: {
-      current: any;
-    };
-    prefs?: {
-      window: Window;
-      columns: Array<ColumnOptions>;
-      rows: Array<{ [dataKey: string]: string }>;
-    };
-    dialog?: DialogHelper;
-  };
-  // Lifecycle hooks
+  public data: Data;
   public hooks: typeof hooks;
-  // APIs
-  public api: object;
+  public api: Record<string, unknown>;
 
   constructor() {
-    this.data = {
+    this.data = this.initializeData();
+    this.hooks = hooks;
+    this.api = {};
+  }
+
+  private initializeData(): Data {
+    return {
       alive: true,
       env: __env__,
       ztoolkit: createZToolkit()
     };
-    this.hooks = hooks;
-    this.api = {};
   }
 }
-
