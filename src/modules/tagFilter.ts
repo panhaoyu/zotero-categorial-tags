ac0d77356cdb7ef6d9bbc0f043c5e1ec9ec13ab3
panhaoyu <panhaoyu.china@outlook.com>
2024-10-26 10:20:11 +0800

    Fix pinyin searcher, for input string, only use the raw format instead of converting to pinyin format and then search. The pinyin conversion should only be used on existing tags.

--- src/modules/tagFilter.ts
+++ src/modules/tagFilter.ts
@@ -1,39 +1,24 @@
-import pinyin from "pinyin"; // 使用拼音库
-// noinspection TypeScriptCheckImport
-import FuzzySearch from "fuzzy-search"; // 使用模糊搜索库
+import pinyin from "pinyin";
+import FuzzySearch from "fuzzy-search";
 
 export class TagFilter {
-  private tags: string[];
-  private lowerCaseTags: string[];
-  private pinyinTags: string[][];
+  private readonly lowerCaseTags: string[];
+  private readonly pinyinTags: string[][];
+  private readonly rawSearcher: FuzzySearch<string>;
+  private readonly pinyinSearcher: FuzzySearch<string[]>;
 
   constructor(tags: string[]) {
-    this.tags = tags;
-    this.lowerCaseTags = tags.map(tag => tag.toLowerCase()); // 预处理成小写
-    // noinspection JSAnnotator
-    this.pinyinTags = tags.map(tag => pinyin(tag, { style: pinyin.STYLE_NORMAL }).flat()); // 转换为拼音
+    this.lowerCaseTags = tags.map(tag => tag.toLowerCase());
+    this.pinyinTags = tags.map(tag => pinyin(tag, { style: pinyin.STYLE_NORMAL }).flat());
+    this.rawSearcher = new FuzzySearch(this.lowerCaseTags, [], { caseSensitive: false });
+    this.pinyinSearcher = new FuzzySearch(this.pinyinTags, [], { caseSensitive: false });
   }
 
   public filterTags(input: string): boolean[] {
     const filterValue = input.toLowerCase();
-
-    // 原始字符串的模糊搜索
-    const searcher = new FuzzySearch(this.lowerCaseTags, [], { caseSensitive: false });
-    const resultFromOriginal = searcher.search(filterValue).map(tag => this.lowerCaseTags.indexOf(tag));
-
-    // 输入汉字时，也将其转换为拼音并进行拼音检索
-    // noinspection JSAnnotator
-    const pinyinInput = pinyin(filterValue, { style: pinyin.STYLE_NORMAL }).flat().join("");
-    const pinyinSearcher = new FuzzySearch(this.pinyinTags, [], { caseSensitive: false });
-    const resultFromPinyin = pinyinSearcher.search(pinyinInput).map(tag => this.pinyinTags.indexOf(tag));
-
-    // 合并去重
+    const resultFromOriginal = this.rawSearcher.search(filterValue).map(tag => this.lowerCaseTags.indexOf(tag));
+    const resultFromPinyin = this.pinyinSearcher.search(filterValue).map(tag => this.pinyinTags.indexOf(tag));
     const resultIndexes = Array.from(new Set([...resultFromOriginal, ...resultFromPinyin]));
-
     return this.lowerCaseTags.map((_, index) => resultIndexes.includes(index));
-  }
-
-  public getOriginalTags(): string[] {
-    return this.tags;
   }
 }
