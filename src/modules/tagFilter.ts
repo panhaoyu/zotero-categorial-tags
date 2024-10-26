af918b6d6aa371b7ab095ac32c0f94cedc3d100f
panhaoyu <panhaoyu.china@outlook.com>
2024-10-26 10:26:44 +0800

    Fix pinyin searcher, for input string, only use the raw format instead of converting to pinyin format and then search. The pinyin conversion should only be used on existing tags.

--- src/modules/tagFilter.ts
+++ src/modules/tagFilter.ts
@@ -2,12 +2,14 @@ import pinyin from "pinyin";
 import FuzzySearch from "fuzzy-search";
 
 export class TagFilter {
+  private readonly rawTags: string[];
   private readonly lowerCaseTags: string[];
   private readonly pinyinTags: string[][];
   private readonly rawSearcher: FuzzySearch<string>;
   private readonly pinyinSearcher: FuzzySearch<string[]>;
 
   constructor(tags: string[]) {
+    this.rawTags = tags;
     this.lowerCaseTags = tags.map(tag => tag.toLowerCase());
     this.pinyinTags = tags.map(tag => pinyin(tag, { style: pinyin.STYLE_NORMAL }).flat());
     this.rawSearcher = new FuzzySearch(this.lowerCaseTags, [], { caseSensitive: false });
@@ -19,6 +21,6 @@ export class TagFilter {
     const resultFromOriginal = this.rawSearcher.search(filterValue).map(tag => this.lowerCaseTags.indexOf(tag));
     const resultFromPinyin = this.pinyinSearcher.search(filterValue).map(tag => this.pinyinTags.indexOf(tag));
     const resultIndexes = Array.from(new Set([...resultFromOriginal, ...resultFromPinyin]));
-    return this.lowerCaseTags.map((_, index) => resultIndexes.includes(index));
+    return this.rawTags.map((_, index) => resultIndexes.includes(index));
   }
 }
